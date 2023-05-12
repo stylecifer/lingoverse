7af9344aa1881c4cccaa2619844ef690e8bf3aa1
Tonito <stylecifer@gmail.com>
2023-05-11 22:10:17 -0400

    Add the text for Millen article

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -2,13 +2,23 @@
 const articles = [
     {
         id: '1',
-        title: 'Millen, Language Exchange Host',
-        slug:'millen-language-exchange-host',
+        title: 'Experience the Magic of Language Exchange with Millen K',
+        slug:'experience-the-magic-of-language-exchange-with-millen-k',
         coverImage: 'https://via.placeholder.com/1200x600',
         videoUrl: 'https://www.youtube.com/embed/JDL8l91m3Zo',
         content: `
-          <p>This is the content of the first article.</p>
-          <p>It can contain HTML tags like <strong>bold</strong> and <em>italic</em>.</p>
+          <p>Hello there! I hope you're doing well. It's Antonio here, but you might know me as 'El Toñito' when we're navigating through the vast expanse of language learning into the Lingoverse.
+
+          I've got a real treat for you today! I recently had the privilege of hopping on board the language spaceship with none other than Mellen K, the illustrious host of our beloved Language Exchange every Sunday at 12 pm New York Time.
+          
+          In this captivating interview, now available as a video for your viewing pleasure, you'll discover Mellen's motivation for learning languages, keeping alive Sunday’s event in meetup and why he believes language exchange is the undefeatable technique for mastering a new tongue.
+          
+          "This is the one folks, the golden ticket to fluency!" Language exchange isn't just about learning a language; it's about immersing yourself in a whole new galaxy – right from the comfort of your own home. In fact, it's like getting front-row seats to a world tour, without having to worry about losing your luggage!
+          
+          Let's make every Sunday a day of discovery in the Lingoverse! 
+          
+          Let me know in the comment if you're down</p>
+          
         `,
       },
       {
